92c3a276f77fceadfa68ac0a34bc57a0fe77cb23
Teresa <teresado.21t@gmail.com>
2019-01-08 22:35:50 -0800

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Container } from './Container'
-export { default as CenterCard } from './CenterCard'
-export { default as ContactIcons } from './ContactIcons'
-export { default as Header } from './Header'
